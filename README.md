e279bcaaaa3e899fc0434d447306ddb42d1ac60c
WX-DongXing <dong.xing@outlook.com>
2026-02-28 18:52:43 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,1 @@
+# PinLens
---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export default function middleware() {
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/((?!.*\\..*|_next).*)",
+        "/(api|trpc)(.*)",
+        "/dashboard(.*)",
+        "/",
+    ],
+};
