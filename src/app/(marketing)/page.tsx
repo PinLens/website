bd58d5282aeffce057f966e48f1d1760795790d4
WX-DongXing <dong.xing@outlook.com>
2026-03-01 01:28:58 +0800

    refactor: 移除未使用的组件

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { LampContainer } from "@/components/ui/lamp";
 import MagicBadge from "@/components/ui/magic-badge";
 import MagicCard from "@/components/ui/magic-card";
-import { COMPANIES, PROCESS } from "@/utils";
+import { PROCESS } from "@/utils";
 import { ArrowRightIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -70,35 +70,6 @@ const HomePage = () => {
                     </AnimationContainer>
                 </div>
             </MaxWidthWrapper >
-
-            {/* Companies Section */}
-            <MaxWidthWrapper>
-                <AnimationContainer delay={0.4}>
-                    <div className="py-14">
-                        <div className="mx-auto px-4 md:px-8">
-                            <h2 className="text-center text-sm font-medium font-heading text-neutral-400 uppercase">
-                                受到设计师和开发者的喜爱
-                            </h2>
-                            <div className="mt-8">
-                                <ul className="flex flex-wrap items-center gap-x-6 gap-y-6 md:gap-x-16 justify-center">
-                                    {COMPANIES.map((company) => (
-                                        <li key={company.name}>
-                                            <Image
-                                                src={company.logo}
-                                                alt={company.name}
-                                                width={80}
-                                                height={80}
-                                                quality={100}
-                                                className="w-28 h-auto"
-                                            />
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </AnimationContainer>
-            </MaxWidthWrapper>
 
             {/* Features Section */}
             <MaxWidthWrapper className="pt-10">
